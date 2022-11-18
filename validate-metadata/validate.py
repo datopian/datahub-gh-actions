939b76db3e5a58aacab51d2c7bb294bd106331d5
Luccas Mateus <luccasmmg@gmail.com>
2022-11-18 15:10:18 -0300

    fix: typo

--- validate-metadata/validate.py
+++ validate-metadata/validate.py
@@ -7,15 +7,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-if os.getenv('DATASET_NAME') is None:
-    raise Exception("Lacking a filename argument")
-filename = os.getenv('DATASET_NAME')
-full_path = f"/home/runner/work/{os.getenv('REPO_NAME')}/{os.getenv('REPO_NAME')}/datasets/{filename}/datapackage.json"
-try:
-    valid = validate(full_path)
-except exceptions.ValidationError as exception:
-    for error in exception.errors:
-    print(error)
-    exit(1)
+    if os.getenv('DATASET_NAME') is None:
+        raise Exception("Lacking a filename argument")
+    filename = os.getenv('DATASET_NAME')
+    full_path = f"/home/runner/work/{os.getenv('REPO_NAME')}/{os.getenv('REPO_NAME')}/datasets/{filename}/datapackage.json"
+    try:
+        valid = validate(full_path)
+    except exceptions.ValidationError as exception:
+        for error in exception.errors:
+        print(error)
+        exit(1)
 
 main()
